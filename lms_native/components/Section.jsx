6af9c32a0d7a7fecd521dd898785df4d6416b1a9
Mostafa Kassem <moustafakassem12@gmail.com>
2021-07-08 12:26:14 +0300

    First Version

---
+++ lms_native/components/Section.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import API from '../api';
+import { View } from "react-native";
+import { Picker } from '@react-native-community/picker';
+
+export default function Section(props) {
+
+    const [sections, setSections] = useState([]);
+    const id = props.id;
+
+    const fetchdata = async id => {
+
+        if (id && id !== "") {
+            await API.get(`classroomsection/${id}`)
+                .then(res => {
+                    const result = res.data.data;
+                    setSections(result);
+                });
+        } else { setSections([]); }
+    }
+
+    useEffect(() => {
+        fetchdata(id);
+    }, [id]);
+
+    return (
+        <View>
+
+            <Picker
+                selectedValue={props.value}
+                onValueChange={props.onValueChange}
+                style={props.style}
+            >
+
+                <Picker.Item
+                    value=""
+                    label="Section"
+                />
+
+                {sections.map(section => (
+                    <Picker.Item
+                        key={section.id}
+                        value={section.id}
+                        label={section.name}
+                    />
+                ))}
+
+            </Picker>
+        </View>
+    );
+}
